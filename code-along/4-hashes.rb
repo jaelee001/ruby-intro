433d6b63baf1c6aa1b352fb8ac03d10889ba3fff
jaelee001 <jaehee.lee@kellogg.northwestern.edu>
2022-01-27 03:06:12 +0000

    lab-4

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,24 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile = {
+    name:"Jae",
+    location: {
+        city: "Columbus",
+        state: "IN"
+    },
+    status: "Student"
+}
+puts profile
+puts profile[:status]
+name = profile [:name]
+puts name
+puts profile[:location][:city]
+puts profile[:location][:state]
 
 # Accessing data from the hash
 
 # More Complex Hashes
+profile[:profession] = "student"
+puts profile
+
